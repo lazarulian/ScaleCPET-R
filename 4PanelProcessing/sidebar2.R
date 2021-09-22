6b298f44f560a9423cb334d728f82892f86c7c9d
lazarulian <69273628+lazarulian@users.noreply.github.com>
2021-09-22 12:24:52 -0700

    Added Navbar Setup
    Included multiple tests for the different graphs that are present.

---
+++ 4PanelProcessing/sidebar2.R
@@ -0,0 +1,4 @@
+list(
+    h2("File Input:"), #TitleSidebar
+    em("Currently only accepting raw xlsx output from Cosmed software.")
+)
--- 4PanelProcessing/ui.R
+++ 4PanelProcessing/ui.R
@@ -1,20 +1,30 @@
-shinyUI(fluidPage(
-    titlePanel("UC-Fitness Lab Data Processing Application"),
-    
-    # Sidebar Layout
-    sidebarLayout( 
-        sidebarPanel(
-            source("sidebar.R", local = TRUE)[1] # Reference Sidebar.R
-        ), #EndSidebarPanel
-        
-        # Main Panel with Graph Output
-        mainPanel(
-            h1("Graph Output"),
-            plotOutput("plot1", width = "1300", height = "1300") # Outputs from server-side
+# UI.R
+shinyUI(navbarPage("UC-Fitness Lab Data Processing Application",
+    tabPanel("4-Panel Plot",
+        sidebarLayout(
+            sidebarPanel(
+                source("sidebar1.R", local = TRUE)[1] # Reference Sidebar1.R
+            ), #EndSidebarPanel
             
-        ),#EndMainPanel
-        
-    ) #EndSidebarLayout
+            # Main Panel with Graph Output
+            mainPanel(
+                h1("Graph Output"),
+                plotOutput("plot1", width = "1300", height = "1300") # Outputs from server-side
+                
+            )#EndMainPanel
+            
+        ) #EndSidebarLayout
+    ), # End Tab Panel
+    tabPanel("Corrected 4Panel",
+        sidebarLayout(
+            sidebarPanel(
+                source("sidebar2.R", local = TRUE)[1] # Reference Sidebar1.R
+            ),
+            mainPanel(
+                h1("Graph Output"),
+            )
+        )
+    )
     
     
-)) #EndFluidpage (Shiny UI Template)
+)) #End NavBarPage (Shiny UI Template)
