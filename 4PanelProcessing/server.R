ee64e6753c1e8163ccad1ec80e2ffeab9c732ce3
lazarulian <69273628+lazarulian@users.noreply.github.com>
2022-04-11 20:07:07 -0700

    Removed Uneccessary Packages and Archived Capture for Deployment

--- 4PanelProcessing/server.R
+++ 4PanelProcessing/server.R
@@ -1,19 +1,12 @@
 # Server.R
 library(shiny)
 library(shinydashboard)
-library(haven)       ## Allows for reading other file formats.
 library(tidyverse)   ## Contains multiple packages that are essential to R. (GGplot2, ForCats, Purrr, Tibble, dplyr, stringr, readr, tidyr)
 library(ggpubr)      ## Publication ready R designs
-library(rstatix)     ## Conducts basic statistical testing 
-library(arsenal)     ## Large scale statistical summaries.
 library(car)         ## Regression Grammar
 library(ggplot2)     ## Grammar of Graphics
 library(readxl)      ## Reads Excel Files
-library(cowplot)     ## Aligns Plot
 library(zoo)         ## Rolling Averages
-library(shiny)       ## Shiny
-library(broom)       ## Linear Regression?
-library(lubridate)
 library(rmarkdown)
 library(remotes)
 library(capture)
--- 4PanelProcessing/ui.R
+++ 4PanelProcessing/ui.R
@@ -33,13 +33,13 @@ shinyUI(dashboardPage(skin = "blue",
     fileInput("file1", "Choose xlsx File",
               multiple = FALSE,
               accept = c(".xlsx"),),#EndFileInput
-    strong(".     ."),
-    capture_pdf(
-            selector = "body",
-            filename = "tabularized_report",
-            scale = 3,
-            icon("camera"), "Download"
-            )
+    strong(".     .")
+    # capture_pdf(
+    #         selector = "body",
+    #         filename = "tabularized_report",
+    #         scale = 3,
+    #         icon("camera"), "Download"
+    #         )
   ),
   
   dashboardBody(
